6559dda906d3ddbc3dedb44f49ca03eb4762eee3
DESKTOP-29VI7GO\user <wnstjd637@gmail.com>
2025-07-10 12:23:01 +0900

    no message

--- backrncs/routes/index.js
+++ backrncs/routes/index.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../schemas/users');
 
-router.get("/",(req,res,next)=> {
-    return res.json({"me":"success"})
+router.post('/register', async (req, res,next) => {
+    try{
+        const {nickname, email, password} = req.body;
+        const user = new User({ email, password, nickname });
+        // insert...
+        const result = await user.save();
+        return res.json(result);
+    }catch(err){
+        return res.status(500).json(err);
+    }
+});
+
+router.get('/', async (req, res,next) => {
+    try{
+        // select....
+        const users = await User.find();
+        return res.json(users);
+    }catch(err){
+        return res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res,next) => {
+    // const id = req.params.id;
+    try{
+        const {id} = req.params;
+        // supabase.from('users').delete().eq('id',id);
+        const deleted = await User.findByIdAndDelete(id);
+        // 정상일때
+        return res.json(deleted);
+    }catch(err){
+        // 에러일때
+        return res.status(500).json(err);
+    }
+
+
 })
 
 module.exports = router;
--- backrncs/schemas/index.js
+++ backrncs/schemas/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-require('dotenv').config(); // process.env env 설정한 내용이 가져온다.
+// 중복제거
+// require('dotenv').config(); // process.env env 설정한 내용이 가져온다.
 
 const mongo_url = process.env.MONGO_URL;
 
