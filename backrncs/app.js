1ba7217c36f732e082836a58ab113fddb56137c6
byeonjoonsung <wnstjd637@gmail.com>
2025-07-14 12:24:40 +0900

    Revert "no message"
    This reverts commit 418bdbce4c6ebf59f3710c8d53a681f9cf64cc87.

--- backrncs/app.js
+++ backrncs/app.js
@@ -13,7 +13,6 @@ const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -42,7 +41,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/auth", authRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
